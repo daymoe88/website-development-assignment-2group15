f198a20b0291be1c8266442a7e7d17365019047e
spen0171 <80034359+spen0171@users.noreply.github.com>
2021-10-22 23:27:56 +1030

    Uploaded product info and shopping cart pages

---
+++ add-to-cart.php
@@ -0,0 +1,53 @@
+<?php
+
+include_once "db-functions.php";
+
+if (isset($_POST['quantity']) && isset($_POST['productID']))
+{
+    // open a new connection to the db
+    require_once "dbconn.php";
+
+    // create a query to add the product to the cart
+    $query = "INSERT INTO CartProduct VALUES (1, ?, ?);";
+
+    // prepare statement to sanitise user input
+    $statement = mysqli_stmt_init($conn);
+    mysqli_stmt_prepare($statement, $query);
+    mysqli_stmt_bind_param($statement, 'ss', htmlspecialchars($_POST['productID']), htmlspecialchars($_POST['quantity']));
+
+    // execute statement
+    if (mysqli_stmt_execute($statement))
+    {
+        // if new item successfully added, redirect back to the product-information page
+        $selectQuery = "SELECT * FROM CartProduct WHERE userID=1 AND productID=?;";
+
+        mysqli_stmt_prepare($statement, $selectQuery);
+        mysqli_stmt_bind_param($statement, 's', htmlspecialchars($_POST['productID']));
+
+        mysqli_stmt_execute($statement);
+
+        $result = mysqli_stmt_get_result($statement);
+        $cartItem = mysqli_fetch_assoc($result);
+
+        // update the stock levels in the database -- NOT YET!! only update stock levels on purchases!!!
+        //update_stock($conn, $cartItem['productID'], $cartItem['quantity']);
+
+        // return to previous page
+        header('location: product-information.php?productID='.$cartItem['productID']);
+    }
+    else
+    {
+        echo "Error: Unable to add item to cart.<br>";
+        error_log(mysqli_error($conn));
+        //echo mysqli_error($conn);
+    }
+
+    // close the prepared statement
+    mysqli_stmt_close($statement);
+
+    // close connection to the db
+    mysqli_close($conn);
+
+}
+
+?>
